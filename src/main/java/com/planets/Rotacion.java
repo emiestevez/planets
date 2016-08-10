83a9733cfabe06852702ab05845e4856a8bc95e2
epeste <emiliano.pablo.estevez@monsanto.com>
2016-08-10 15:20:04 -0300

    remove comment

--- src/main/java/com/planets/Rotacion.java
+++ src/main/java/com/planets/Rotacion.java
@@ -1,9 +1,6 @@
 package com.planets;
 
 public abstract class Rotacion {
-    //Rotacion(-1, 360, 0),
-    //Rotacion(1, 0, 360);
-
     private final int gradosInicio;
 
     private final int gradosFin;
